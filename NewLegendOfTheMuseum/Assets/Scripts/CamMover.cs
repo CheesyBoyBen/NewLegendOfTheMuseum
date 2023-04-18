e37174b2b0c76b00062bcb5b3af0ee8d9c9b2657
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2023-04-18 17:35:56 +0100

    created CamMover and set up example at com2 dolly clone

--- NewLegendOfTheMuseum/Assets/Scripts/CamMover.cs
+++ NewLegendOfTheMuseum/Assets/Scripts/CamMover.cs
@@ -4,29 +4,54 @@ using UnityEngine;
 
 public class CamMover : MonoBehaviour
 {
-    public Vector3 TargetPos;
-    public Vector3 TargetRot;
+    public Vector3 targetPos;
+    public Vector3 targetRot;
+    public GameObject cam;
+    private camera camScript;
+    private bool inRadius;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camScript = cam.GetComponent<camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((inRadius) && (Input.GetKeyDown(KeyCode.E)))
+        {
+            playerEnter();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRadius = true;
+
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inRadius = false;
+            playerExit();
+        }
     }
 
     void playerEnter()
     {
-
+        camScript.MoveToPos(targetPos, targetRot);
     }
 
     void playerExit()
     {
-
+        camScript.MoveToDefault();
     }
 }
