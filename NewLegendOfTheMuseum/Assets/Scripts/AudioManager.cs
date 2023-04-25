2cece2c129d2f5b7711b11101048579c91cb27ac
DESKTOP-OUAALTE\benja <benjaminlockhart@hotmail.co.uk>
2023-04-25 19:22:31 +0100

    Revert "made global volume slider"
    This reverts commit 772847eb759d10a70fa7fd9f8a8bd22da9708d01.

--- NewLegendOfTheMuseum/Assets/Scripts/AudioManager.cs
+++ NewLegendOfTheMuseum/Assets/Scripts/AudioManager.cs
@@ -1,23 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public AudioSource audioSource;
-    public Slider volumeSlider;
+    public AudioClip startClip;
 
-    public static float volume = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!PlayerPrefs.HasKey("musicVolume"))
-        {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
-        }
+
     }
 
     // Update is called once per frame
@@ -28,21 +22,7 @@ public class AudioManager : MonoBehaviour
 
     public void PlayAudio(AudioClip newAudio)
     {
+        audioSource.clip = newAudio;
         audioSource.Play();
-    }
-
-    public void ChangeVolume()
-    {
-        AudioListener.volume = volumeSlider.value;
-    }
-
-    private void Load()
-    {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
-    }
-
-    private void Save()
-    {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
