612cc1fb5f84489d913382b9471ad6aaf68dd04d
MattMoho <61742137+MattMoho@users.noreply.github.com>
2023-04-27 11:18:11 +0100

    Revert "added comic books"
    This reverts commit afe11b3ca8209088405548685bafea808b1556ee.

--- NewLegendOfTheMuseum/Assets/PlayVideoAndQuit.cs
+++ NewLegendOfTheMuseum/Assets/PlayVideoAndQuit.cs
@@ -4,33 +4,14 @@ using UnityEngine.Video;
 public class PlayVideoAndQuit : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public string url;
 
     private void Start()
     {
-        videoPlayer = GetComponent<VideoPlayer>();
-        videoPlayer.url = url;
         videoPlayer.loopPointReached += EndReached;
         videoPlayer.Play();
     }
 
-    void Update()
-    {
-        if (Input.anyKey)
-        {
-            Play();
-        }
-
-    }
-
-    void Play()
-    {
-        videoPlayer.Play();
-        videoPlayer.isLooping = true;
-    }
-
-
-private void EndReached(UnityEngine.Video.VideoPlayer vp)
+    private void EndReached(UnityEngine.Video.VideoPlayer vp)
     {
         Application.Quit();
     }
